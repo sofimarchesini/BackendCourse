510bb20a4df4a991bee7b53319c8441317f63e4b
Sofimarchesini <sofiamarchesini@hotmail.com>
2022-05-10 18:54:44 -0300

    adding desafio4

--- desafio3/Container.js
+++ desafio3/Container.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+import fs from 'fs';
 
 class Container
 {
@@ -33,7 +33,10 @@ class Container
         return prod["id"]
     }
 
-    getById(id) {return this.cont.find(obj => obj.id === id || null)}
+    getById(id) {
+        try { return this.cont.find(obj => obj.id === id || null);}
+        catch(error) {console.log("The id is not correct");}
+    }
 
     getAll() {
       this.readFile();
@@ -47,6 +50,11 @@ class Container
     deleteAll() {
         this.cont = ""
         this.writeFile()
+    }
+
+    updateById(id,prod) { 
+        try { this.cont[this.cont.findIndex(obj => obj.id === id)] = prod;}
+        catch(error) {console.log(error, ":producto no encontrado");}
     }
 }
 
---
+++ desafio4/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import Container from './Container.js';
+
+const app = express();
+const router = express.Router();
+const cont = new Container('productos.json')
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use('/api/productos', router);
+
+router.get('/',  (req, res) => {
+    console.log("Mostrando productos");
+    res.status(200).json(cont.getAll())
+})
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    cont.getById(id) ? res.status(200).json(cont.getById(id)) : res.status(404).json({error: "producto no encontrado"});
+})
+
+router.post('/', (req,res) => {
+    const {prod} = req;
+    res.status(200).send("producto agregado")
+})
+
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const {prod} = req;
+    cont.updateById(id,prod) ? res.status(200).send("producto actualizado") : res.status(404).send("El id es incorrecto");
+})
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    cont.deleteById(id) ? res.status(200).send("producto borrado") : res.status(404).send("El id es incorrecto");
+})
+
+const server = app.listen(8080, () => {console.log(`server started on port 8080`)})
+
+server.on('error', (err) => console.log(err));
