d086f99976fdfcee81a67b28a1b38c1377457ab9
Sofimarchesini <sofiamarchesini@hotmail.com>
2022-08-12 14:03:32 -0300

    Heorku

--- ProyectoFinal/src/index.js
+++ ProyectoFinal/src/index.js
@@ -3,23 +3,7 @@ import  app from "./app.js";
 import http from "http";
 import {hideBin} from "yargs/helpers";
 
-yargs(hideBin(process.argv)).command({
-  command: "add port",
-  describe: "add number of port",
-  builder: {
-    PORT: {
-      describe: "number of port",
-      demandOption: true,
-      type: "number",
-    },
-  },
-  handler(argv) {
-    console.log(`PORT is: ${argv.PORT}`);
-  },
-}).parse();
-
-
-const PORT =  8000;
+const PORT = process.env.PORT | 8080;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
