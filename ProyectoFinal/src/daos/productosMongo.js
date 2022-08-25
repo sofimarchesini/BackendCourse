911a8c55433e5dd2f7eaca97e52214427f3d90ae
sofimarchesini <64851357+sofimarchesini@users.noreply.github.com>
2022-08-25 19:48:54 -0300

    Delete ProyectoFinal/src directory

--- ProyectoFinal/src/daos/productosMongo.js
+++
@@ -1,9 +0,0 @@
-import MongoClass from "../Components/MongoClass.js";
-import { productosSchema } from "../schemas/productoSchema.js";
-
-class ProductoDaoMongo extends MongoClass {
-    
-    constructor() { super("productos", productosSchema); }
-}
-
-export default ProductoDaoMongo
